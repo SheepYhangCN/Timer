c4bff26d4d724d5d8963dfe55df88bb547b14e00
SheepYhangCN <sheepyuhang@163.com>
2024-04-10 18:03:25 +0800

    language

--- main.cs
+++ main.cs
@@ -14,13 +14,17 @@ public partial class main : Control
 	internal bool start=false;
 	public override void _Ready()
 	{
-		if (OS.GetLocaleLanguage() == "zh" || OS.GetLocale() == "zh_CN" || OS.GetLocale() == "zh_SG")
-		{
-		    TranslationServer.SetLocale("zh_CN");
-		}
 		if (OS.GetLocale() == "zh_TW" || OS.GetLocale() == "zh_HK" || OS.GetLocale() == "zh_MO")
 		{
 		    TranslationServer.SetLocale("zh_TW");
+		}
+		else if (OS.GetLocaleLanguage() == "zh" || OS.GetLocale() == "zh_CN" || OS.GetLocale() == "zh_SG")
+		{
+		    TranslationServer.SetLocale("zh_CN");
+		}
+		else
+		{
+		    TranslationServer.SetLocale(OS.GetLocale());
 		}
 	}
 
